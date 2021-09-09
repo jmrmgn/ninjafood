e1ec87a7bcdae62941923bbd7292c69302ebace1
Jomar <jomar.magno026@gmail.com>
2021-09-09 17:04:33 +0800

    Add Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,18 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#008080',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
